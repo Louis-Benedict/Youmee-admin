94d3cfb40c4cf52c090eb3d4d9d2c3b9f128b078
louis benedict <info@louis-benedict.de>
2023-10-13 23:56:59 +0700

    Lint fixes

--- app/api/v1/presign/route.ts
+++ app/api/v1/presign/route.ts
@@ -1,20 +1,34 @@
-import { NextResponse } from 'next/server';
-import preSignFile from '@/app/libs/s3';
-import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server'
+import preSignFile from '@/app/libs/s3'
+import { getServerSession } from 'next-auth'
+import { ApiError } from 'next/dist/server/api-utils'
+import { HttpStatusCode } from 'axios'
 
-export async function POST(request: Request) {
-	try {
-		const session = await getServerSession();
-		if (!session) return NextResponse.error();
+export async function generatePresignedUrl(request: Request) {
+    const session = await getServerSession()
+    if (!session) {
+        throw new ApiError(
+            HttpStatusCode.InternalServerError,
+            'Enrollee could not be promoted'
+        )
+    }
 
-		const body = await request.json();
-		console.log('FIIIIIIILE SSSSEEEEEEEEERVER: ' + body);
-		const { fileType } = body;
+    const body = await request.json()
+    const { fileType } = body
 
-		const { postUrl, getUrl, fields, key } = await preSignFile(fileType, 'image');
+    const { postUrl, getUrl, fields, key } = await preSignFile(
+        fileType,
+        'image'
+    )
 
-		return NextResponse.json({ postUrl, getUrl, fields, key });
-	} catch (error) {
-		console.log(error);
-	}
+    return NextResponse.json(
+        {
+            message: 'Success',
+            status: 200,
+            data: [{ postUrl, getUrl, fields, key }],
+        },
+        { status: 200 }
+    )
 }
+
+export const POST = generatePresignedUrl
