c5ce43ed0e933ebb79ac0efa30ef02e0853d540f
louis benedict <info@louis-benedict.de>
2023-10-23 12:09:04 +0700

    added branch

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,31 @@
 const nextJest = require('next/jest')
 const { pathsToModuleNameMapper } = require('ts-jest')
 const { compilerOptions } = require('./tsconfig')
+import type { Config } from 'jest'
 
 const createJestConfig = nextJest({
     dir: './',
 })
 
-const customJestConfig = {
+const customJestConfig: Config = {
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     modulePaths: ['<rootDir>/app'],
     testEnvironment: 'jest-environment-jsdom',
-    testPathIgnorePatterns: ['/node_modules', '/__tests__/utils'],
+    testPathIgnorePatterns: ['/node_modules', '/__tests__/utils', '/app'],
     verbose: true,
+    collectCoverageFrom: [
+        '<rootDir>/app/components/*.{ts,tsx}',
+        '<rootDir>/app/(**)/*.{ts,tsx}',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 50,
+            functions: 50,
+            lines: 50,
+            statements: 50,
+        },
+    },
 }
 
 module.exports = createJestConfig(customJestConfig)
