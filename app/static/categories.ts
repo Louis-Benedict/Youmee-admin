0b3ccc9c84c9af6aa5a8ee25b0cad3bc817306ea
louis benedict <info@louis-benedict.de>
2023-10-24 12:18:17 +0700

    deployment workflow

--- app/static/categories.ts
+++ app/static/categories.ts
@@ -1,5 +1,4 @@
 import { Category } from '@prisma/client'
-import { useTranslations } from 'next-intl'
 import { ClientCategory } from '../types'
 
 export function isInCategories(input: string) {
@@ -8,40 +7,39 @@ export function isInCategories(input: string) {
 
 export const getCategories = (): ClientCategory[] => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const t = useTranslations('common.categories')
     return [
         {
             id: '1',
             label: Category.ACTOR,
-            plural: t('actor'),
+            plural: 'actor',
             slug: '/browse/actor',
             color: 'primary-dark',
         },
         {
             id: '2',
             label: Category.MUSICIAN,
-            plural: t('musician'),
+            plural: 'musician',
             slug: '/browse/musician',
             color: 'primary-dark',
         },
         {
             id: '3',
             label: Category.COMEDIAN,
-            plural: t('comedian'),
+            plural: 'comedian',
             slug: '/browse/comedian',
             color: 'primary-dark',
         },
         {
             id: '4',
             label: Category.ATHLETE,
-            plural: t('athlete'),
+            plural: 'athlete',
             slug: '/browse/athlete',
             color: 'primary-dark',
         },
         {
             id: '5',
             label: Category.SOCIAL_MEDIA,
-            plural: t('influencer'),
+            plural: 'influencer',
             slug: '/browse/social-media',
             color: 'primary-dark',
         },
