afeeacf75282e038a8d79f393ff04749be43d53f
louis benedict <info@louis-benedict.de>
2023-11-19 22:32:20 +0700

    cicd

--- __scripts__/noDowntimeDeployment.sh
+++ __scripts__/noDowntimeDeployment.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 docker pull $1/youmee:latest
-RUNNING_CONTAINER=$(docker container ps -q | wc -l | grep $1/youmee)
+RUNNING_CONTAINER=$(docker container ls -f name=app -q)
 
 if [[ -z $RUNNING_CONTAINER ]]; then 
     echo "No running container found. Starting containers..."
-    docker-compose up -d
+    docker-compose -f ../docker-compose.yaml up -d
+    test $? -eq 0 || echo "Failed to start containers"
 else 
     echo "Containers already running. Attempting hot reload..."
-    docker-compose scale web=2 --no-recreate
+    docker-compose scale youmee_admin=2 --no-recreate
 fi
 
 sleep 10
 docker rm -f youmee_old
-docker-compose up -d -f ../docker --scale web=1 --no-recreate
+docker-compose -f ../docker-compose.yaml up -d --scale youmee_admin=1 
+
+test $? -eq 0 || echo "Failed to scale containers"
