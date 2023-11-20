26e2d84414c7d6c4161050aa29156c049813ae27
louis benedict <info@louis-benedict.de>
2023-11-20 21:47:41 +0700

    cicd

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,15 @@
  * @type {import('next').NextConfig}
  */
 
+import { NextConfig } from 'next'
+
 const s3Host = process.env.NEXT_PUBLIC_S3_URL
 const cfHost = process.env.NEXT_PUBLIC_CF_URL
 const host = process.env.HOST
 const port = process.env.PORT
 const protocol = process.env.PROTOCOL
 
-const nextConfig = {
+const nextConfig: NextConfig = {
     reactStrictMode: true,
     compiler: {
         styledComponents: true,
@@ -22,15 +24,15 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: s3Host,
+                hostname: s3Host!,
             },
             {
                 protocol: 'https',
-                hostname: cfHost,
+                hostname: cfHost!,
             },
             {
-                protocol: protocol,
-                hostname: host,
+                protocol: protocol! as 'http' | 'https',
+                hostname: host!,
                 port: port,
             },
         ],
