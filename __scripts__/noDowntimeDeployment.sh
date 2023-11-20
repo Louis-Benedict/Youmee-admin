957057613f1268c60c674ae913338315c57f2ed5
louis benedict <info@louis-benedict.de>
2023-11-21 00:23:06 +0700

    cicd

--- __scripts__/noDowntimeDeployment.sh
+++ __scripts__/noDowntimeDeployment.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
+reload_nginx() {  
+  docker exec app-proxy /usr/sbin/nginx -s reload  
+}
+
 docker pull $1/youmee:latest
 RUNNING_CONTAINER=$(docker container ls -f name=app -q)
 
 cd ~/app
-
-if [[ -z $RUNNING_CONTAINER ]]; then 
-    echo "No running container found. Starting containers..."
-    docker-compose -f ../docker-compose.yaml up -d
-    (test $? -eq 0 || echo "Failed to start containers") > 2&
-else 
-    echo "Containers already running. Attempting hot reload..."
-    docker-compose scale admin-app=2 --no-recreate
-fi
-
-echo "Waiting for container startup..."
+service_name=admin-app
+old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
+docker-compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
+new_container_id=$(docker ps -f name=$service_name -q | head -n1)
+new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
 sleep 10
+reload_nginx
+echo "Waiting for container startup..."
+
 echo "Deleting old container..."
-docker rm -f app-admin-app-1
-docker-compose -f ../docker-compose.yaml up -d --scale admin-app=1 
-docker container prune -f
+docker stop $old_container_id
+docker rm $old_container_id
+docker-compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
+
+ 
+  reload_nginx  
 
 (test $? -eq 0 || echo "Failed to scale containers") > 2&
