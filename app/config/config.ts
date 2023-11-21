4c8c5f811291cef7e77c0d027dd53ad18a2b789a
louis benedict <info@louis-benedict.de>
2023-11-21 22:08:03 +0700

    cicd

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,12 +1,10 @@
 export function getConfig() {
     return Object.freeze({
         baseUrl: process.env.NEXT_PUBLIC_BASEURL!,
-        apiBaseUrl:
-            process.env.NEXT_PUBLIC_BASEURL! +
-            process.env.NEXT_PUBLIC_API_BASEURL!,
+        apiBaseUrl: process.env.NEXT_PUBLIC_API_BASEURL!,
         cdnBaseUrl: process.env.NEXT_PUBLIC_CF_URL!,
         s3BaseUrl: process.env.NEXT_PUBLIC_S3_URL!,
-        emailAddress: process.env.EMAIL_SENDER!,
+        emailAddress: process.env.EMAIL_SENDER,
     })
 }
 
