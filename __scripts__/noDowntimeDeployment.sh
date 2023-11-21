5bbac60d834ea0e25993f281edd55490ad0435e5
louis benedict <info@louis-benedict.de>
2023-11-21 17:02:54 +0700

    cicd

--- __scripts__/noDowntimeDeployment.sh
+++ __scripts__/noDowntimeDeployment.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
+service_name=admin-app
+proxy_name=app-proxy-1
 
-reload_nginx() {  
-  docker exec app-proxy /usr/sbin/nginx -s reload  
+reload_nginx() {
+  echo 'Reloading proxy config...'
+  docker exec $proxy_name /usr/sbin/nginx -s reload  
 }
 
 docker pull $1/youmee:latest
-RUNNING_CONTAINER=$(docker container ls -f name=app -q)
+
 
 cd ~/app
-service_name=admin-app
 old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
 docker-compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
 new_container_id=$(docker ps -f name=$service_name -q | head -n1)
 new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
+
+echo "Waiting for container startup..."
 sleep 10
 reload_nginx
-echo "Waiting for container startup..."
 
 echo "Deleting old container..."
 docker stop $old_container_id
@@ -23,6 +26,6 @@ docker rm $old_container_id
 docker-compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
 
  
-  reload_nginx  
+reload_nginx  
 
 (test $? -eq 0 || echo "Failed to scale containers") > 2&
--- app/config/config.ts
+++ app/config/config.ts
@@ -1,12 +1,13 @@
 export function getConfig() {
-    return {
+    return Object.freeze({
         baseUrl: process.env.NEXT_PUBLIC_BASEURL!,
         apiBaseUrl:
             process.env.NEXT_PUBLIC_BASEURL! +
             process.env.NEXT_PUBLIC_API_BASEURL!,
         cdnBaseUrl: process.env.NEXT_PUBLIC_CF_URL!,
         s3BaseUrl: process.env.NEXT_PUBLIC_S3_URL!,
-    }
+        emailAddress: process.env.EMAIL_SENDER,
+    })
 }
 
 export const config = getConfig()
