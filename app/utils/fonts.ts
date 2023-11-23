8cc16454f4ec9d3e5bfb88f3b643b254dcd509a1
louis benedict <info@louis-benedict.de>
2023-11-23 22:10:20 +0700

    cicd

--- app/utils/fonts.ts
+++ app/utils/fonts.ts
@@ -1,18 +1,7 @@
-import { Albert_Sans, Inter, Noto_Sans_Thai, Prompt } from 'next/font/google'
+import { Inter } from 'next/font/google'
 
 export const default_font_eng = Inter({
     subsets: ['latin'],
     style: ['normal'],
     weight: ['300', '400', '600', '700'],
-})
-// export const default_font_thai = Noto_Sans_Thai({
-// 	subsets: ['thai'],
-// 	style: ['normal'],
-// 	weight: ['300', '400', '600'],
-// });
-
-export const second_font_thai = Prompt({
-    subsets: ['thai', 'latin-ext'],
-    style: ['normal'],
-    weight: ['300', '400', '600'],
 })
